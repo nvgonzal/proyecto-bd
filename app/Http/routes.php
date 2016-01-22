5ba12137446427bd842134a6785836a29653ab44
Nicolas Gonzalez Tardon <nvgonzal@alumnos.ubiobio.cl>
2016-01-22 12:55:56 -0300

    Agrega rutas de consultas y CRUD cliente

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('cliente','ClientesController');
+
+Route::get('/consulta1','ConsultaController@consulta1');
+
+Route::get('/consulta2','ConsultaController@consulta2');
+
+Route::get('/consulta3','ConsultaController@consulta3');
+
+Route::get('/consulta5','ConsultaController@consulta5');
+
+Route::get('/consulta6','ConsultaController@consulta6');
+
+Route::get('/consulta7','ConsultaController@consulta7');
